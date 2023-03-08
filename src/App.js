ba5d7c1e5b987889a58130790ef2f4664145b5dd
Kyle Millar <kyledmillar@gmail.com>
2023-03-08 11:56:51 +0000

    Create login form and question template

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       question: "question",
-      answers: ["answer1", "answer2", "answer3"]
+      answers: []
     }
-  }
-  update(e) {
-    this.setState({question: e.target.value})
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   render() {
-    return (
-      <div>
+    let myComponent;
+    if (this.state.answers.length == 0) {
+      myComponent = (
+        <div>
         <h1>Supporting Integration</h1>
-        <h2>{this.state.question}</h2>
-        <ul>
-          {this.state.answers.map((ans) => (
-            <li key={ans}>
-              <input type="checkbox" name="check" value={ans}
-              onClick={e => this.onlyOne(e.target.value)} />{ans}
-            </li>
-          )
-          )}
-        </ul>
-      </div>
-    )
+
+        <DetailsForm handleSubmit={this.handleSubmit}></DetailsForm>
+        <div>{this.state.answers}</div>
+
+        </div>
+      )
+    }
+    else {
+      myComponent = (
+        <div>
+          <h1>Supporting Integration</h1>
+          <h2>{this.state.question}</h2>
+          <form>
+            {this.state.answers.map((ans) => (
+              <div key={ans}>
+                <input type="radio" id={ans} name="choice" value={ans} />
+                <label htmlFor={ans}>{ans}</label>
+              </div>
+              )
+            )}
+            <input type="submit" value="&rarr;"></input>
+          </form>
+        </div>
+      )
+    }
+
+    return myComponent
+
   }
-  onlyOne(checkedAns) {
-    let checkboxes = document.getElementsByName("check")
-    checkboxes.forEach((item) => {
-      if (item.value !== checkedAns) {
-        item.checked = false
-      }
-    })
+  handleSubmit() {
+    console.log("test")
+    this.setState({answers: ["answer1", "answer2", "answer3", "test"]})
+    
   }
 }
 
-
+class DetailsForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form id="details">
+          <label htmlFor="fname">First name:</label>
+          <input type="text" id="fname" placeholder="First name"></input>
+        
+          <label htmlFor="lname">Last name:</label>
+          <input type="text" id="lname" placeholder="Last name"></input>
+        
+          <label htmlFor="role">Role:</label>
+          <input type="text" id="role" placeholder="Role"></input>
+        
+          <label htmlFor="desc">Short description of you:</label>
+          <input type="text" id="desc" placeholder="Description"></input>
+          
+          <input type="submit" value="Start" onClick={this.props.handleSubmit}></input>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default App
