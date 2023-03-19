d0adeac11f00c08e75dc0081d1835d9f9703714b
Kyle Millar <kyledmillar@gmail.com>
2023-03-19 21:18:35 +0000

    Create results screen

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import { initializeApp } from "firebase/app";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { waitForElementToBeRemoved } from '@testing-library/react';
 
+import "./index.css"
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDVN5G7GDHLTOY7i37RgN9OrmsZtY40YII",
-  authDomain: "honours-project-476b0.firebaseapp.com",
-  projectId: "honours-project-476b0",
-  storageBucket: "honours-project-476b0.appspot.com",
-  messagingSenderId: "468964246212",
-  appId: "1:468964246212:web:2d6e67a0251d58d974ff91",
-  measurementId: "G-TJ19SJLYLN"
+    apiKey: "AIzaSyDVN5G7GDHLTOY7i37RgN9OrmsZtY40YII",
+    authDomain: "honours-project-476b0.firebaseapp.com",
+    projectId: "honours-project-476b0",
+    storageBucket: "honours-project-476b0.appspot.com",
+    messagingSenderId: "468964246212",
+    appId: "1:468964246212:web:2d6e67a0251d58d974ff91",
+    measurementId: "G-TJ19SJLYLN"
 }
 
 const app = initializeApp(firebaseConfig)
@@ -24,39 +26,39 @@ const usersRef = collection(db, "users")
 const scenariosRef = collection(db, "scenarios")
 
 class App extends React.Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      question: "question",
-      answers: [],
-      user: ""
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.setUser = this.setUser.bind(this)
-  }
-
-  async handleSubmit() {
-    // this.state.answers = ["answer1", "answer2", "answer3", "test"]
-    const q = query(scenariosRef)
-    const querySnap = await getDocs(q).then((querySnap) => querySnap.forEach((doc) => {
-      this.state.answers.push(doc.data())}))
     
-  }
+    constructor() {
+        super();
+        this.state = {
+            question: "question",
+            answers: [],
+            user: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.setUser = this.setUser.bind(this)
+    }
 
-  setUser(docRef) {
-    this.setState({user: docRef.id})
-  }
+    async handleSubmit() {
+        // this.state.answers = ["answer1", "answer2", "answer3", "test"]
+        const q = query(scenariosRef)
+        const querySnap = await getDocs(q).then((querySnap) => querySnap.forEach((doc) => {
+            this.state.answers.push(doc.data())}))
+        
+    }
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.user == "" ? 
-        <HomePage handleSubmit={this.handleSubmit} setUser={this.setUser} answers={this.answers}/> 
-        : <Questions answers={this.state.answers} user={this.state.user}/>}
-      </div>
-    )
-  }
+    setUser(docRef) {
+        this.setState({user: docRef.id})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.user == "" ? 
+                <HomePage handleSubmit={this.handleSubmit} setUser={this.setUser} answers={this.answers}/> 
+                : <Questions answers={this.state.answers} user={this.state.user}/>}
+            </div>
+        )
+    }
 }
 
 
@@ -66,106 +68,106 @@ class App extends React.Component {
 
 class HomePage extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.setUser = this.setUser.bind(this)
-  }
+    constructor(props) {
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.setUser = this.setUser.bind(this)
+    }
 
-  handleSubmit() {
-    this.props.handleSubmit();
-  }
+    handleSubmit() {
+        this.props.handleSubmit();
+    }
 
-  setUser(docRef) {
-    this.props.setUser(docRef);
-  }
+    setUser(docRef) {
+        this.props.setUser(docRef);
+    }
 
-  render() {
-    return (
-      <div className="HomePage">
-        <h1>Supporting Integration</h1>
+    render() {
+        return (
+            <div className="home-page">
+                <h1>Supporting Integration</h1>
 
-        <DetailsForm handleSubmit={this.handleSubmit} setUser={this.setUser}></DetailsForm>
-      </div>
-    )
-  }
+                <DetailsForm handleSubmit={this.handleSubmit} setUser={this.setUser}></DetailsForm>
+            </div>
+        )
+    }
 }
 
 
 
 
 class DetailsForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      first_name: "",
-      last_name: "",
-      role: "",
-      description: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name: "",
+            last_name: "",
+            role: "",
+            description: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleFname = this.handleFname.bind(this)
+        this.handleLname = this.handleLname.bind(this)
+        this.handleRole = this.handleRole.bind(this)
+        this.handleDesc = this.handleDesc.bind(this)
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleFname = this.handleFname.bind(this)
-    this.handleLname = this.handleLname.bind(this)
-    this.handleRole = this.handleRole.bind(this)
-    this.handleDesc = this.handleDesc.bind(this)
-  }
-  async sendUserData(e) {
-    e.preventDefault();
-    const docRef = await addDoc(collection(db, "users"), {
-      first_name: this.state.first_name,
-      last_name: this.state.last_name,
-      role: this.state.role,
-      description: this.state.description
-    }).then((docRef) => {this.props.setUser(docRef)})
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const docRef = this.sendUserData(e)
-    this.props.handleSubmit(docRef)
-  }
-  handleFname(e) {
-    this.state.first_name = e.target.value
-  }
-  handleLname(e) {
-    this.state.last_name = e.target.value
-  }
-  handleRole(e) {
-    this.state.role = e.target.value
-  }
-  handleDesc(e) {
-    this.state.description = e.target.value
-  }
-  render() {
-    return (
-      <div>
-        <form id="details">
-          <div id="fname">
-            <label htmlFor="fname">First name:</label>
-            <input onChange={this.handleFname} type="text" id="fname" placeholder="First name"></input>
-          </div>
+    async sendUserData(e) {
+        e.preventDefault();
+        const docRef = await addDoc(collection(db, "users"), {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            role: this.state.role,
+            description: this.state.description
+        }).then((docRef) => {this.props.setUser(docRef)})
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const docRef = this.sendUserData(e)
+        this.props.handleSubmit(docRef)
+    }
+    handleFname(e) {
+        this.state.first_name = e.target.value
+    }
+    handleLname(e) {
+        this.state.last_name = e.target.value
+    }
+    handleRole(e) {
+        this.state.role = e.target.value
+    }
+    handleDesc(e) {
+        this.state.description = e.target.value
+    }
+    render() {
+        return (
+            <div>
+                <form id="details">
+                    <div id="fname">
+                        <label htmlFor="fname">First name:</label>
+                        <input onChange={this.handleFname} type="text" id="fname" placeholder="First name"></input>
+                    </div>
 
-          <div id="lname">
-            <label htmlFor="lname">Last name:</label>
-            <input onChange={this.handleLname} type="text" id="lname" placeholder="Last name"></input>
-          </div>
+                    <div id="lname">
+                        <label htmlFor="lname">Last name:</label>
+                        <input onChange={this.handleLname} type="text" id="lname" placeholder="Last name"></input>
+                    </div>
 
-          <div id="role">
-            <label htmlFor="role">Role:</label>
-            <input onChange={this.handleRole} type="text" id="role" placeholder="Role"></input>
-          </div>
+                    <div id="role">
+                        <label htmlFor="role">Role:</label>
+                        <input onChange={this.handleRole} type="text" id="role" placeholder="Role"></input>
+                    </div>
 
-          <div id="desc">
-            <label htmlFor="desc">Short description of you:</label>
-            <input onChange={this.handleDesc} type="text" id="desc" placeholder="Description"></input>
-          </div>
+                    <div id="desc">
+                        <label htmlFor="desc">Short description of you:</label>
+                        <input onChange={this.handleDesc} type="text" id="desc" placeholder="Description"></input>
+                    </div>
 
-          <div id="submit-button">
-            <input type="submit" value="Start" onClick={this.handleSubmit}></input>
-          </div>
-        </form>
-      </div>
-    )
-  }
+                    <div id="submit-button">
+                        <input type="submit" value="Start" onClick={this.handleSubmit}></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 
@@ -173,73 +175,157 @@ class DetailsForm extends React.Component {
 
 
 class Questions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      question: "question",
-      answers: this.props.answers,
-      user: this.props.user,
-      tags: [],
-      curChoice: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: "question",
+            answers: this.props.answers,
+            user: this.props.user,
+            tags: [],
+            curChoice: "",
+            userString: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
 
-  handleSubmit(e) {
-    console.log(e)
-    e.preventDefault();
+    handleSubmit(e) {
+        e.preventDefault();
 
-    this.state.answers.forEach((v) => {
-      
-      if (this.state.curChoice == v["key"]) {
+        this.state.answers.forEach((v) => {
+            
+            if (this.state.curChoice == v["key"]) {
 
-        let toAdd = []
-        v["add_tags"].forEach((e) => { 
-          if (!this.state.tags.includes(e)) {
-            toAdd.push(e)
-          }
-        })
-        
-        let removedArr = this.state.tags
-        v["remove_tags"].forEach((e) => {
-          removedArr = removedArr.filter(x => x != e)
-        })
+                let toAdd = []
+                v["add_tags"].forEach((e) => { 
+                    if (!this.state.tags.includes(e)) {
+                        toAdd.push(e)
+                    }
+                })
+                
+                let removedArr = this.state.tags
+                v["remove_tags"].forEach((e) => {
+                    removedArr = removedArr.filter(x => x != e)
+                })
 
-        let temp = toAdd.concat(removedArr)
-        this.setState({ tags: temp })
-      }
-    })
-    console.log(this.state.tags)
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Supporting Integration</h1>
-        <h2>{this.state.question}</h2>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          {this.state.answers.map((ans) => {
-            return (
-              ans["condition"].every(v => this.state.tags.includes(v)) &&
-              ans["false_condition"].every(v => !this.state.tags.includes(v)) &&
-              (
-              <div key={ans["key"]}>
-                <input required type="radio" onClick={() => this.state.curChoice=ans["key"]} id={ans["key"]} name="choice" value={ans["key"]} />
-                <label htmlFor={ans["key"]}>{ans["choice"]}</label>
-              </div>
-              )
-            )
+                let temp = toAdd.concat(removedArr)
+                this.setState({ tags: temp })
             }
-          )
-          }
-          
-          <input type="submit" value="&rarr;"></input>
-        </form>
-      </div>
-    )
-  }
+        })
+        this.setState({ userString: this.state.userString + this.state.curChoice})
+        console.log(this.state.userString)
+    }
+
+    render() {
+        return (
+
+            <div>
+                {this.state.userString.length < 5 ?
+                    <div>
+                        <h1>Supporting Integration</h1>
+                        <h2>{this.state.question}</h2>
+                        <form onSubmit={(e) => this.handleSubmit(e)}>
+                            {this.state.answers.map((ans) => {
+                                return (
+                                    ans["condition"].every(v => this.state.tags.includes(v)) &&
+                                    ans["false_condition"].every(v => !this.state.tags.includes(v)) &&
+                                    (
+                                    <div key={ans["key"]}>
+                                        <input required type="radio" onClick={() => this.state.curChoice=ans["key"]} id={ans["key"]} name="choice" value={ans["key"]} />
+                                        <label htmlFor={ans["key"]}>{ans["choice"]}</label>
+                                    </div>
+                                    )
+                                )
+                                }
+                            )
+                            }
+                            
+                            <input type="submit" value="&rarr;"></input>
+                        </form>
+                    </div>
+                    :
+                    <AnswerScreen userString={this.state.userString} user_id={this.state.user} />
+                }
+            </div>
+        )
+    }
 }
 
+
+
+class AnswerScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            userString: this.props.userString,
+            user_id: this.props.user_id,
+            users: []
+        }
+        this.getUsers = this.getUsers.bind(this)
+        this.setUsers = this.setUsers.bind(this)
+        
+        
+        console.log("state id:",this.state.user_id)
+    }
+
+    async componentDidMount() {
+        await this.getUsers()
+    }
+
+    async getUsers() {
+        const q = query(usersRef)
+        await getDocs(q).then((querySnap) => querySnap.forEach((doc) => {this.setUsers(doc)}))
+    }
+
+    setUsers(user) {
+        let temp = this.state.users
+        temp.push(user)
+        this.setState({ users: temp })
+    }
+
+    render() {
+        return (
+            <div id="object-container">
+                {this.state.users.map((user, index) => {
+                    console.log(user.id)
+                    return (
+                        user.id != this.state.user_id && user.id != "placeholder" &&
+                        <UserObject key={index} first_name={user.data()["first_name"]} last_name={user.data()["last_name"]} role={user.data()["role"]} description={user.data()["description"]} />
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+
+
+class UserObject extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            first_name: this.props.first_name,
+            last_name: this.props.last_name,
+            role: this.props.role,
+            description: this.props.description
+        }
+        console.log(this.state.first_name)
+    }
+    render() {
+        return (
+            <div className="user-object">
+                <div id="answer-name">
+                    <b>Name:</b> {this.state.first_name + " " + this.state.last_name}
+                </div>
+                <div id="answer-role">
+                    <b>Role:</b> {this.state.role}
+                </div>
+                <div id="answer-desc">
+                    <b>Description:</b> {this.state.description}
+                </div>
+            </div>
+        )
+    }
+}
 
 
 export default App
